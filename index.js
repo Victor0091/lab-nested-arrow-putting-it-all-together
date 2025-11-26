a82bf6b0b873192babcc485bae2fc1b46a91b25b
kibetkoech <vickibelangs@gmail.com>
2025-11-26 19:37:06 +0300

    passed with walikng colors

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function createLoginTracker(user) {
+  let failed = 0;
+  let locked = false;
 
+  return (passwordAttempt) => {
+    if (locked) {
+      return 'Account locked due to too many failed login attempts';
+    }
 
+    if (passwordAttempt === user.password) {
+      return 'Login successful';
+    }
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+    failed += 1;
+    if (failed <= 3) {
+      return `Attempt ${failed}: Login failed`;
+    }
+
+    locked = true;
+    return 'Account locked due to too many failed login attempts';
+  };
+}
+
+module.exports = { createLoginTracker };
